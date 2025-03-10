b17f9dedf873356e7e667c527b167b212b9fe96c
mica-1999 <micael1999work@gmail.com>
2025-03-10 14:48:55 +0000

    vercel testing

---
+++ next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  // Enable static exports if you're using static site generation
+  // output: 'export',
+}
+
+module.exports = nextConfig
